7777fff064ebd3f0f98b829ce8fce5bc4a67e5a8
Carlos Becker <carlosbecker@gmail.com>
2014-03-26 22:59:13 +0100

    Fixes PCA (issue #13) and adds necessary tests

--- test/pca.jl
+++ test/pca.jl
@@ -28,3 +28,25 @@ X = X * R
 cov(X)
 
 results = pca(X)
+
+
+###  Tests related to issue #13  ###
+
+# to compare columns of A and B, regardless of sign swapping
+function mycomp( A, B ) 
+  err = 0.0
+  for i=1:size(A,1)
+    err += min( sum(abs( A[:,i] - B[:,i] )), sum(abs( A[:,i] + B[:,i] )) )
+  end
+
+  return err
+end
+
+srand(1238)
+X = randn(200,4) * rand(4,4)
+
+testMatrix = { mycomp( pcasvd(X, scale=sc, center=ctr).rotation, 
+				pcaeig(X, scale=sc, center=ctr).rotation )
+  for sc=[true,false], ctr=[true,false] }
+
+@assert all( testMatrix .< 1e-12 )
